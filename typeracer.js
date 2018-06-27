ac089579289998cd4e66367e32ef06134c0c4890
IdoRozilio <xxidoxx662@gmail.com>
2018-06-27 05:28:50 +0300

    Create typeracer.js

---
+++ typeracer.js
@@ -0,0 +1,29 @@
+/****************************************************************************************************************
+* Run it in the chrome console before the game starts, press space whenever you want it to fill the word for you.
+*****************************************************************************************************************/
+
+var last = document.getElementsByClassName("inputPanel")[0].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].getElementsByTagName("div")[0].getElementsByTagName("div")[0];
+var text = "";
+
+try {
+	text = last.getElementsByTagName("span")[0].innerHTML + last.getElementsByTagName("span")[1].innerHTML + last.getElementsByTagName("span")[2].innerHTML
+}
+catch{
+ text = last.getElementsByTagName("span")[0].innerHTML + last.getElementsByTagName("span")[1].innerHTML
+}
+
+console.log(text)
+words = text.split(" ");
+
+var num = 0;
+function print_next() {
+    document.getElementsByTagName("input")[0].value = words[num];
+    num++;
+
+}
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+       print_next();
+    }
+}
